7ca02db1260c6be0d60510f16107277b0dfcc7ce
govind <joshigovind740@gmail.com>
2024-04-21 13:58:16 +0530

    firt

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './globle.css'
+const App = () => {
+  return (
+    <div>
+          <h1 >
+              Hello
+      </h1>
+    </div>
+  )
+}
+
+export default App
